4b41c6e28ef6f7a7354a7831be6806a590751229
Tyler Flint <tylerflint@gmail.com>
2023-01-05 13:49:34 -0500

    Allow appUrl to be set dynamically via state

--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,20 @@ export interface ProxyConfig {
 // adapter registration
 export default function proxy(config: ProxyConfig) {
   // return middleware
-  return async function run(context: Context, next: Function) {
+  return async function run(ctx: Context, next: Function) {
     // extract the config
-    const { appUrl, basePath = '' } = config
+    let { appUrl, basePath = '' } = config
+
+    // allow runtime config to specify appUrl
+    if (ctx.state['appUrl'])
+      appUrl = ctx.state['appUrl']
+
+    // allow runtime config to specify basePath
+    if (ctx.state['basePath'])
+      basePath = ctx.state['basePath']
 
     // proxy the request
-    await proxyRequest(appUrl, context, { basePath })
+    await proxyRequest(appUrl, ctx, { basePath })
 
     // continue along
     return next()
