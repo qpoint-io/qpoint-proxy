afa87c26729300e2d8b892a8ac5e91f9af9913d2
Tyler Flint <tylerflint@gmail.com>
2023-12-12 10:01:33 -0500

    Ensure the proxy strips the trailing slash for both origin and to

--- src/url.ts
+++ src/url.ts
@@ -11,9 +11,13 @@ function replaceOrigin(from: string, to: string): string {
   // extract the origin
   let { origin } = new URL(from)
 
-  // remove the trailing slash
+  // remove the trailing slash from the origin
   if (origin.endsWith('/'))
     origin = origin.slice(0, -1)
+
+  // remove the trailing slash from to
+  if (to.endsWith('/'))
+    to = to.slice(0, -1)
 
   // rebase the URL
   return from.replace(origin, to)
